ebf7bc453c08be7e79a35bec4d49b7d9b3a60c5c
TOPWAL Surjeet <surjeet.singh@soprasteria.com>
2025-07-08 16:32:50 +0530

    rg add kiya ha

--- parent modules/main.tf
+++ parent modules/main.tf
@@ -6,7 +6,7 @@ module "resource_group" {
 
 module "resource_group" {
   source                  = "../modules/azurerm_resource_group"
-  resource_group_name     = "rg-jeet-101"
+  resource_group_name     = "rg-jeet-shi"
   resource_group_location = "centralindia"
 }
 
