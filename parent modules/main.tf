9972af1b618048e301b206dafc526f66425b19c9
surjeet topwal <topwal.surjeet2015@gmail.com>
2025-07-10 15:44:09 +0530

    surjeet ne push kiya ha

--- parent modules/main.tf
+++ parent modules/main.tf
@@ -4,6 +4,11 @@ module "resource_group" {
   resource_group_location = "centralindia"
 }
 
+module "resource_group" {
+  source                  = "../modules/azurerm_resource_group"
+  resource_group_name     = "rg-jeet-surjeet"
+  resource_group_location = "centralindia"
+}
 
 module "virtual_network" {
   depends_on               = [module.resource_group]
