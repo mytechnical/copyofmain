fef80be21963d492b6991712ec5a912f44f5928f
surjeet topwal <topwal.surjeet2015@gmail.com>
2025-07-10 15:18:33 +0530

    simran ne change kiya ha

--- parent modules/main.tf
+++ parent modules/main.tf
@@ -4,6 +4,11 @@ module "resource_group" {
   resource_group_location = "centralindia"
 }
 
+module "resource_group" {
+  source                  = "../modules/azurerm_resource_group"
+  resource_group_name     = "rg-simran-new"
+  resource_group_location = "centralindia"
+}
 
 module "virtual_network" {
   depends_on               = [module.resource_group]
