ece4ae9dbaa0d212fbd041aad1c7ebf636ba7963
TOPWAL Surjeet <surjeet.singh@soprasteria.com>
2025-07-11 13:36:28 +0530

    conflict change kiya ha

--- parent modules/main.tf
+++ parent modules/main.tf
@@ -6,7 +6,7 @@ module "resource_group" {
 
 module "resource_group" {
   source                  = "../modules/azurerm_resource_group"
-  resource_group_name     = "rg-jeet-cobflict"
+  resource_group_name     = "rg-jeet-cobflict-new change kiya ha"
   resource_group_location = "centralindia"
 }
 
